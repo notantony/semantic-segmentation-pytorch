0003a109b96481822e6c039ccb1278214683c28d
notantony <not.antony@yandex.ru>
2020-04-26 16:27:27 +0300

    Fix b64.sh utility

--- b64.sh
+++ b64.sh
@@ -1,1 +1,1 @@
-python -c "import base64; s = open(\"$1\", \"rb\").read(); open(\"tmp.txt\", \"wb\").write(base64.encodebytes(s))"
+python -c "import base64; s = open(\"$1\", \"rb\").read(); open(\"tmp.txt\", \"w\").write(base64.encodebytes(s).decode(\"UTF-8\").replace(\"\\n\", \"\"))"
